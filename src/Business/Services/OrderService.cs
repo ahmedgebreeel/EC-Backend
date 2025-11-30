583f6b4e81e008af5ffe18b303a9662d24f53e5c
NoranTarekf953 <ntsm2001@gmail.com>
2025-11-30 12:42:43 +0200

    Add orders endpoints

---
+++ src/Business/Services/OrderService.cs
@@ -0,0 +1,108 @@
+﻿using Core.DTOs.OrderItems;
+using Core.DTOs.Orders;
+using Core.Entities;
+using Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class OrderService
+    {
+        UnitOfWork unitOfWork;
+        
+        public OrderService(UnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
+
+        public async Task<IEnumerable<OrderDTO>> GetAllAsync()
+        {
+            var orders = await unitOfWork.Orders.GetAllAsync();
+            var orderDto = orders.Select(o => new OrderDTO
+            {
+                Id = o.Id,
+                UserId = o.UserId,
+                UserName = o.User.Name,
+                Status = o.Status,
+                Total = o.Total,
+                AddressId = o.AddressId,
+                Address = o.Address,
+                OrderItems = o.OrderItems.Select(oi => new OrderItemsDto
+                {
+                    Id = oi.Id,
+                    OrderId = oi.Id,
+                    ProductId = oi.ProductId,
+                    Product = oi.Product,   
+                    Price = oi.Price,
+                    Quantity = oi.Quantity,
+                }).ToList()
+            }).ToList();
+            return orderDto;
+        }
+
+        public async Task<OrderDTO?> GetByIdAsync(string id)
+        {
+            var order = await unitOfWork.Orders.GetByIdAsync(id);
+            if(order == null)
+            {
+                return null;
+            }
+            var orderDto = new OrderDTO
+            {
+                Id = order.Id,
+                UserId = order.UserId,
+                UserName = order.User.Name,
+                Status = order.Status,
+                Total = order.Total,
+                AddressId = order.AddressId,
+                Address = order.Address,
+                OrderItems = order.OrderItems.Select(oi => new OrderItemsDto
+                {
+                    Id = oi.Id,
+                    OrderId = oi.Id,
+                    ProductId = oi.ProductId,
+                    Product = oi.Product,
+                    Price = oi.Price,
+                    Quantity = oi.Quantity,
+                }).ToList()
+
+            };
+            return orderDto;
+        }
+
+        public async Task<OrderDTO?> GetByUserIdAsync(string userId)
+        {
+            var order = await unitOfWork.Orders.GetByUserIdAsync(userId);
+            if (order == null)
+            {
+                return null;
+            }
+            var orderDto = new OrderDTO
+            {
+                Id = order.Id,
+                UserId = order.UserId,
+                UserName = order.User.Name,
+                Status = order.Status,
+                Total = order.Total,
+                AddressId = order.AddressId,
+                Address = order.Address,
+                OrderItems = order.OrderItems.Select(oi => new OrderItemsDto
+                {
+                    Id = oi.Id,
+                    OrderId = oi.Id,
+                    ProductId = oi.ProductId,
+                    Product = oi.Product,
+                    Price = oi.Price,
+                    Quantity = oi.Quantity,
+                }).ToList()
+
+            };
+            return orderDto;
+
+        }
+    }
+}
---
+++ src/Data/Repositories/OrderRepo.cs
@@ -0,0 +1,48 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class OrderRepo : GenericRepository<Order>
+    {
+        public OrderRepo(AppDbContext _context) : base(_context)
+        {
+        }
+
+        public override async Task<IEnumerable<Order>> GetAllAsync(int? pageNum = null, int? pageSize = null)
+        {
+
+           return await context.Orders
+                .Include(o => o.User)
+                .Include(o=>o.Address)
+                .Include(o => o.OrderItems)
+                    .ThenInclude(oi => oi.Product)
+                .ToListAsync();
+        }
+
+        public override async Task<Order?> GetByIdAsync(string id)
+        {
+            return await context.Orders
+                .Include(o => o.User)
+                .Include(o => o.Address)
+                .Include(o => o.OrderItems)
+                    .ThenInclude(oi => oi.Product)
+                .FirstOrDefaultAsync(o => o.Id == id);
+        }
+
+        public async Task<Order?> GetByUserIdAsync(string userId)
+        {
+            return await context.Orders
+                .Include(o => o.User)
+                .Include(o => o.Address)
+                .Include(o => o.OrderItems)
+                    .ThenInclude(oi => oi.Product)
+                .FirstOrDefaultAsync(o => o.UserId == userId);
+        }
+    }
+}
