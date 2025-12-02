d85fd36f45bc21bb21466c76cbaef7a3698c0bf3
NoranTarekf953 <ntsm2001@gmail.com>
2025-12-02 14:22:09 +0200

    Added orderItems dtos

---
+++ src/Business/Services/OrderItemService.cs
@@ -0,0 +1,31 @@
+﻿
+using Core.DTOs.OrderItems;
+using Data.Repositories;
+
+namespace Business.Services
+{
+    public class OrderItemService
+    {
+        private readonly UnitOfWork unitOfWork;
+        public OrderItemService(UnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
+
+        public async Task AddAsync(AddOrderItemsDto addOrderItemDto)
+        {
+            var existingOrder = await unitOfWork.Orders.GetByIdAsync(addOrderItemDto.OrderId);
+            if (existingOrder == null) { 
+                throw new InvalidOperationException("Order does not exist.");
+            }
+            var existingProduct = await unitOfWork.Products.GetByIdAsync(addOrderItemDto.ProductId);
+            if (existingProduct == null) {
+                throw new InvalidOperationException("Product does not exist.");
+            }
+
+
+        }
+
+
+    }
+}
---
+++ src/Core/DTOs/OrderItems/UpdateOrderItemDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.OrderItems
+{
+    public class UpdateOrderItemDto
+    {
+        [Required]
+        public int Quantity { get; set; } = 1;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
