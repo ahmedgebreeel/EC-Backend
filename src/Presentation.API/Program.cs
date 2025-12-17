95b838614aa7ea12bc324f4b24144064a6b498b8
ahmedgebreeel <ahmedgerbeeeel@gmail.com>
2025-12-17 07:35:57 +0200

    allow swagger in prod

--- src/Presentation.API/Program.cs
+++ src/Presentation.API/Program.cs
@@ -101,15 +101,15 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-   {
-       c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-       c.RoutePrefix = "";
-   });
-}
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+   c.RoutePrefix = "";
+});
+
 
 app.UseHttpsRedirection();
 
